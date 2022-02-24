1ad69187580619d102b3cfd5583eb0dd52bff36c
savicst <savicst@ssfs.org>
2022-02-24 09:36:50 -0500

    completed creating 2d array practice

--- src/FourByFour.java
+++ src/FourByFour.java
@@ -1,16 +1,62 @@
 public class FourByFour {
     public static void main(String [] args) {
 //code to define an initially empty int 2D array, minimum size 4x4
-
+int [][] willPenn = new int[4][4];
 //code to output all original values
+        System.out.println(willPenn[0][0]);
+        System.out.println(willPenn[0][1]);
+        System.out.println(willPenn[0][2]);
+        System.out.println(willPenn[0][3]);
+        System.out.println(willPenn[1][0]);
+        System.out.println(willPenn[1][1]);
+        System.out.println(willPenn[1][2]);
+        System.out.println(willPenn[1][3]);
+        System.out.println(willPenn[2][0]);
+        System.out.println(willPenn[2][1]);
+        System.out.println(willPenn[2][2]);
+        System.out.println(willPenn[2][3]);
+        System.out.println(willPenn[3][0]);
+        System.out.println(willPenn[3][1]);
+        System.out.println(willPenn[3][2]);
+        System.out.println(willPenn[3][3]);
 
 //code to change half the values to even positive numbers
-
+        willPenn[0][0] = 2;
+        willPenn[0][1] = 6;
+        willPenn[0][2] = 44;
+        willPenn[0][3] = 34;
+        willPenn[1][0] = 48;
+        willPenn[1][1] = 30;
+        willPenn[1][2] = 3094;
+        willPenn[1][3] = 4;
 //and the other half to negative odd numbers
-
+        willPenn[2][0] = -1;
+        willPenn[2][1] = -25;
+        willPenn[2][2] = -20093;
+        willPenn[2][3] = -329;
+        willPenn[3][0] = -953;
+        willPenn[3][1] = -31;
+        willPenn[3][2] = -247;
+        willPenn[3][3] = -34;
 //code to output a blank line separating output sets
-
+        System.out.println(" ");
 //code to output all array elements
+        System.out.println(willPenn[0][0]);
+        System.out.println(willPenn[0][1]);
+        System.out.println(willPenn[0][2]);
+        System.out.println(willPenn[0][3]);
+        System.out.println(willPenn[1][0]);
+        System.out.println(willPenn[1][1]);
+        System.out.println(willPenn[1][2]);
+        System.out.println(willPenn[1][3]);
+        System.out.println(willPenn[2][0]);
+        System.out.println(willPenn[2][1]);
+        System.out.println(willPenn[2][2]);
+        System.out.println(willPenn[2][3]);
+        System.out.println(willPenn[3][0]);
+        System.out.println(willPenn[3][1]);
+        System.out.println(willPenn[3][2]);
+        System.out.println(willPenn[3][3]);
     }
 }
 
--- src/ThreeByThree.java
+++ src/ThreeByThree.java
@@ -1,3 +1,5 @@
+
+
 public class ThreeByThree {
     public static void main(String [] args){
 
@@ -8,17 +10,41 @@ public class ThreeByThree {
 
 //code to output all original values
         System.out.println(list[0][0]);
-
+        System.out.println(list[0][1]);
+        System.out.println(list[0][2]);
+        System.out.println(list[1][0]);
+        System.out.println(list[1][1]);
+        System.out.println(list[1][2]);
+        System.out.println(list[2][0]);
+        System.out.println(list[2][1]);
+        System.out.println(list[2][2]);
 //code to change five of the values to positive
+        list[0][0] = 1;
+        list[0][1] = 2;
+        list[0][2] = 3;
+        list[1][0] = 55;
+        list[1][1] = 830;
 
 
 //and the other four to negative
-
+        list[1][2] = -430;
+        list[2][0] = -43;
+        list[2][1] = -79;
+        list[2][2] = -80;
 
 //code to output a blank line separating output sets
-
+        System.out.println(" ");
 
 //code to output all array elements
+        System.out.println(list[0][0]);
+        System.out.println(list[0][1]);
+        System.out.println(list[0][2]);
+        System.out.println(list[1][0]);
+        System.out.println(list[1][1]);
+        System.out.println(list[1][2]);
+        System.out.println(list[2][0]);
+        System.out.println(list[2][1]);
+        System.out.println(list[2][2]);
     }
 }
 
